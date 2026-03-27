e46775787fbed3bb204a64106f4b85dfe96a5f77
Mateus Ribas <marcalmateus315@gmail.com>
2026-03-27 17:57:35 -0300

    Criaçao estrutura components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Home'
 function App() {
  
 
   return (
     <>
-      <h1>a</h1>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+       
+      </Routes>
+    </BrowserRouter>
+
     </>
   )
 }
