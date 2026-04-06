8a5829a8ac70626fa8476bdd44f267d85277de7e
Arthur Vieira Lopes <arthur.lopes@feluma.org.br>
2026-04-06 11:02:23 -0300

    Começo da página de gestão de residência

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./PagesCliente/Home/Home";
-import Login from './Login/Login'
+import Login from "./Login/Login";
 import Cadastro from "./Cadastro/Cadastro";
+import Gestao from "./GestaoDeResidencia/Gestao";
 function App() {
- 
-
   return (
     <>
-     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-         <Route path="/Login" element={<Login />} />
-           <Route path="/Cadastro" element={<Cadastro />} />
-       
-      </Routes>
-    </BrowserRouter>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Cadastro" element={<Cadastro />} />
+          <Route path="/Gestao" element={<Gestao />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/GestaoDeResidencia/Gestao.jsx
@@ -0,0 +1,15 @@
+import styles from "../GestaoDeResidencia/Gestao.module.css";
+
+function Gestao() {
+  return (
+    <>
+      <main className={styles.principal}>
+        <div className={styles.caixa}>
+          <h1 className={styles.titulo}> Gestão de Residência</h1>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Gestao;
