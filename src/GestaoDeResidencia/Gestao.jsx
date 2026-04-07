4182f01bbc000e09b559887c59f8ff8c54df0738
Arthur Vieira Lopes <arthur.lopes@feluma.org.br>
2026-04-07 09:42:22 -0300

    Alterações na página de gestão

--- src/GestaoDeResidencia/Gestao.jsx
+++ src/GestaoDeResidencia/Gestao.jsx
@@ -1,13 +1,11 @@
 import styles from "../GestaoDeResidencia/Gestao.module.css";
+import Header from "../Components/Header/Header";
 
 function Gestao() {
   return (
     <>
-      <main className={styles.principal}>
-        <div className={styles.caixa}>
-          <h1 className={styles.titulo}> Gestão de Residência</h1>
-        </div>
-      </main>
+      <Header />
+      <main className={styles.principal}></main>
     </>
   );
 }
